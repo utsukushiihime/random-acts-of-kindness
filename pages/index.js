ec5cee224d0f74a17330571deaca3a68a3cdbcaf
Crystal McNeil <crystalsmcneil@gmail.com>
2023-12-19 12:07:09 -0700

    adds analytics

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import Head from 'next/head';
 import KindnessGenerator from '../components/KindnessGenerator';
 import { actsOfKindness } from '../actsOfKindness';
-import { Analytics } from '@vercel/analytics/react';
 
 const Title = styled.h1`
   color: red;
@@ -12,21 +11,18 @@ const Title = styled.h1`
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <Head>
-          <title>Random Acts of Kindness Generator</title>
-          <meta name="description" content="Generate random acts of kindness for the holiday season" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div>
+      <Head>
+        <title>Random Acts of Kindness Generator</title>
+        <meta name="description" content="Generate random acts of kindness for the holiday season" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main>
-          <Title>Random Acts of Kindness Generator</Title>
-          <KindnessGenerator actsOfKindness={ actsOfKindness } />
+      <main>
+        <Title>Random Acts of Kindness Generator</Title>
+        <KindnessGenerator actsOfKindness={ actsOfKindness } />
 
-        </main>
-      </div>
-      <Analytics />
-    </>
+      </main>
+    </div>
   );
 }
